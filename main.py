c2cc731a008c31dfd97167f3e68b23e907a070a4
Giovanni Ancora <ancora.giovanni.scuola+github@gmail.com>
2024-11-15 10:48:57 +0100

    Primo commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello from Flask!'
+
+@app.route('/prova')
+def prova():
+    return '<h1>Pagina di prova</h1>'
+
+app.run(host='localhost', debug=True)
